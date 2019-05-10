0918672932534c24fc2a407d6536638a41157afb
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-10 21:22:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ let next = node => {
   let nextAddress = node[1]
   return collection[nextAddress]
 }
+
+let nodeAt = (head, address) => {
+  let node = head;
+  for (i = 0; i < address; i++) {
+    node = next(node);
+  }
+}
+
+let addressAt = (head, address) => {
+  
+}
