5a218b1e956f667f3741bd94aa215287a347c16e
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-10 21:19:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ let getName = (node) => {
 let headNode = (linkedList, collection) => {
   return collection[linkedList]
 }
+
+let next = node => {
+  let nextAddress = node[1]
+  return collection[nextAddress]
+}
